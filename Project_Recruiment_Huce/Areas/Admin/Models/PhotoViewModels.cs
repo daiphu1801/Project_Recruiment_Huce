656a674c0ba15460e00d4870dc8ff2b4cb6843dd
kevodanh11o2 <ocshitcho@gmail.com>
2025-11-06 23:15:37 +0700

    da them profile photo

--- Project_Recruiment_Huce/Areas/Admin/Models/PhotoViewModels.cs
+++ Project_Recruiment_Huce/Areas/Admin/Models/PhotoViewModels.cs
@@ -1,4 +1,6 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.Web;
 
 namespace Project_Recruiment_Huce.Areas.Admin.Models
 {
@@ -12,7 +14,32 @@ namespace Project_Recruiment_Huce.Areas.Admin.Models
         public string FilePath { get; set; }
         public int? SizeKB { get; set; }
         public string MimeType { get; set; }
-        public DateTime UploadedAt { get; set; }
+        public DateTime? UploadedAt { get; set; }
+
+        public HttpPostedFileBase FileUpload { get; set; }
+
+    }
+    public class CreatePhotoVm
+    {
+
+        public int PhotoId { get; set; }
+        public string FileName { get; set; }
+        public string FilePath { get; set; }
+        public int? SizeKB { get; set; }
+        public string MimeType { get; set; }
+        public DateTime? UploadedAt { get; set; }
+        public HttpPostedFileBase FileUpload { get; set; }
+    }
+
+    public class DeletePhotoVm
+    {
+        public int PhotoId { get; set; }
+        public string FileName { get; set; }
+        public string FilePath { get; set; }
+        public int? SizeKB { get; set; }
+        public string MimeType { get; set; }
+        public DateTime? UploadedAt { get; set; }
+        public HttpPostedFileBase FileUpload { get; set; }
     }
 }
 
